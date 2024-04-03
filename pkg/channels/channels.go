108dff6cf3c4d2bfc935019f80892534da7c3cd6
Otávio Celani <celani.silva@gmail.com>
2024-04-03 18:29:31 -0300

    pkg channels features

--- pkg/channels/channels.go
+++ pkg/channels/channels.go
@@ -20,3 +20,27 @@ func (m *Mux[T]) NewSink() chan T {
 	m.sinks = append(m.sinks, sink)
 	return sink
 }
+
+// Len returns the number of consumers.
+func (m *Mux[T]) Len() int {
+	return len(m.sinks)
+}
+
+// Close closes all consumers.
+func (m *Mux[T]) Close() {
+	for _, sink := range m.sinks {
+		close(sink)
+	}
+	m.sinks = nil
+}
+
+// CloseSink closes a specific consumer.
+func (m *Mux[T]) CloseSink(sink chan T) {
+	for i, s := range m.sinks {
+		if s == sink {
+			close(s)
+			m.sinks = append(m.sinks[:i], m.sinks[i+1:]...)
+			return
+		}
+	}
+}
--- pkg/channels/channels_test.go
+++ pkg/channels/channels_test.go
@@ -9,15 +9,39 @@ import (
 // TestMux tests Mux type usability.
 func TestMux_Send(t *testing.T) {
 	m := &Mux[int]{}
+	defer m.Close()
+
 	sink1 := m.NewSink()
 	sink2 := m.NewSink()
 
 	want := 1
 	go m.Send(want)
-
 	got1 := <-sink1
 	got2 := <-sink2
 
 	assert.Equal(t, want, got1)
 	assert.Equal(t, want, got2)
+}
+
+// TestMux_Close tests Mux type usability.
+func TestMux_CloseSink(t *testing.T) {
+	m := &Mux[int]{}
+	sink1 := m.NewSink()
+	sink2 := m.NewSink()
+
+	want := 2
+	got := m.Len()
+	assert.Equal(t, want, got)
+
+	m.CloseSink(sink1)
+	want = 1
+	got = m.Len()
+	assert.Equal(t, want, got)
+	<-sink1
+
+	m.CloseSink(sink2)
+	want = 0
+	got = m.Len()
+	assert.Equal(t, want, got)
+	<-sink2
 }
