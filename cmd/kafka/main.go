7f91eea835da9017268da53976eaf3dc2a812a5e
Otávio Celani <celani.silva@gmail.com>
2024-04-05 17:19:18 -0300

    kafka app logs and env

--- cmd/kafka/main.go
+++ cmd/kafka/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"circle/pkg/kafka"
+	"circle/pkg/logger"
 	"context"
 	"flag"
-	"fmt"
 
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/twmb/franz-go/pkg/kgo"
 )
 
@@ -13,22 +16,43 @@ import (
 const (
 	SeedBroker    = "localhost:9092"
 	ConsumerGroup = "my-group-identifier"
-)
-
-// Kafka topics.
-const (
-	KAFKA_TOPIC_1 = "first"
-	KAFKA_TOPIC_2 = "second"
+	LogLevel      = zerolog.DebugLevel // logLevel is the global log level for the application.
 )
 
 // kafkaClientOpts returns Kafka client options.
-func kafkaClientOpts(consumeTopic string) []kgo.Opt {
+func kafkaClientOpts(config *KafkaClientConfig) []kgo.Opt {
 	return []kgo.Opt{
-		kgo.SeedBrokers(SeedBroker),
-		kgo.ConsumerGroup(ConsumerGroup),
-		kgo.ConsumeTopics(consumeTopic),
+		kgo.SeedBrokers(config.SeedBroker),
+		kgo.ConsumerGroup(config.ConsumerGroup),
+		kgo.ConsumeTopics(config.Topic1),
 		kgo.AutoCommitMarks(),
 	}
+}
+
+// logReceivedMessage logs the received message.
+func logReceivedMessage(r *kgo.Record) {
+	log.Info().
+		Str("topic", r.Topic).
+		Int32("partition", r.Partition).
+		Int64("offset", r.Offset).
+		Bytes("key", r.Key).
+		Bytes("value", r.Value).
+		Msg("received message")
+}
+
+func logProducedMessage(r *kgo.Record) {
+	log.Info().
+		Str("topic", r.Topic).
+		Bytes("value", r.Value).
+		Msg("producing to")
+}
+
+// init initializes the logger.
+func init() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+	logger.Setup(LogLevel)
 }
 
 // main function.
@@ -44,13 +68,18 @@ func main() {
 	flag.StringVar(&msg, "m", "", "message to send to Kafka")
 	flag.Parse()
 
+	log.Info().Msg("kafka app starting...")
+
+	// Kafka client configuration
+	cfg := NewKafkaClientConfig()
+
 	// consume topic1 by default
-	if consume == "" {
-		consume = KAFKA_TOPIC_1
+	if consume != "" {
+		cfg.Topic1 = consume
 	}
 
 	// create Kafka client
-	c, err := kafka.NewClient(kafkaClientOpts(consume)...)
+	c, err := kafka.NewClient(kafkaClientOpts(cfg)...)
 	if err != nil {
 		panic(err)
 	}
@@ -60,15 +89,17 @@ func main() {
 
 	// produce message if key or msg flags are not empty
 	if msg != "" || key != "" {
-		err := c.ProduceMessage(ctx, &kgo.Record{
-			Topic: KAFKA_TOPIC_1,
+		rec := &kgo.Record{
+			Topic: cfg.Topic1,
 			Key:   []byte(key),
 			Value: []byte(msg),
-		})
-		if err != nil {
+		}
+		if err := c.ProduceMessage(ctx, rec); err != nil {
 			panic(err)
 		}
+		logProducedMessage(rec)
 	}
+	log.Info().Str("topic", cfg.Topic1).Msg("consuming from")
 
 	// poll fetches from Kafka topic
 	fetch, err := c.PollFetches(ctx)
@@ -79,16 +110,15 @@ func main() {
 	// consume messages from Kafka topic
 	err = c.ConsumeMessage(ctx, fetch, func(p kgo.FetchTopicPartition) {
 		for _, r := range p.Records {
-			fmt.Printf(
-				"received message: topic=%s partition=%d offset=%d key=%s value=%s\n",
-				r.Topic, r.Partition, r.Offset, r.Key, r.Value,
-			)
-			// redirect message from first topic to second topic
-			if consume == KAFKA_TOPIC_1 {
-				r.Topic = KAFKA_TOPIC_2
+			logReceivedMessage(r)
+
+			// if consuming from topic1, send the received message to topic2
+			if consume != cfg.Topic1 {
+				r.Topic = cfg.Topic2
 				if err := c.ProduceMessage(ctx, r); err != nil {
 					panic(err)
 				}
+				logProducedMessage(r)
 			}
 		}
 	})
